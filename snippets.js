d2fdb462ea84b1a8eeff5098dde6a1308f08683d
Allan <32887387+AllanCrazyProgrammer@users.noreply.github.com>
2018-11-10 14:36:56 -0600

    deleted snippets

--- snippets.js
+++
@@ -1,7 +0,0 @@
-$("input[type='radio']").each(function (index, radioButton) {
-	console.log($(radioButton).is(":checked"))
-})
-
-$("input[type='radio']").each(function (radioButton) {
-	console.log(radioButton)
-})
